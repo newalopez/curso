eca8d468146a1f4593c7f61a57cbaf676fad0216
Carlos Lopez <newalopez@gmail.com>
2020-08-18 09:13:52 -0600

    - Fix liquibase - Add coverage

--- src/test/java/com/is4tech/curso/curso/CursoApplicationTests.java
+++ src/test/java/com/is4tech/curso/curso/CursoApplicationTests.java
@@ -3,17 +3,23 @@ package com.is4tech.curso.curso;
 import com.is4tech.curso.curso.domain.bo.Status;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@TestPropertySource("classpath:application.properties")
 class CursoApplicationTests {
 
     @Test
     void dummyTest() {
         Status status = new Status();
         status.setId(1);
-        assertEquals(status.getId(), 1);
+        status.setDescription("hola");
+        status.setName("name");
+        assertEquals(1, status.getId());
+        assertEquals("hola", status.getDescription());
+        assertEquals("name", status.getName());
     }
 
 }
