786bcabc73c8debc76250318a63314beed41d19a
Carlos Lopez <newalopez@gmail.com>
2020-08-21 08:56:06 -0600

    - add eureka server

--- src/main/java/com/is4tech/curso/curso/http/health/Adapter.java
+++ src/main/java/com/is4tech/curso/curso/http/health/Adapter.java
@@ -1,15 +1,27 @@
 package com.is4tech.curso.curso.http.health;
 
 import com.is4tech.curso.curso.service.HealthServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class Adapter implements HealthServices {
+    private static final String DEMO2_URL = "http://demo2/api/items/100";
+    private static final Logger logger = LoggerFactory.getLogger(Adapter.class);
+    private final RestTemplate restTemplate;
+
+    public Adapter(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @Override
     public boolean isOk() {
-        // apache http client connector
-        // RestTemplate - spring
-        return true;
+        ResponseEntity<String> exchange = restTemplate.exchange(DEMO2_URL, HttpMethod.GET, null, String.class);
+        logger.info("http status: {}, message {}.", exchange.getStatusCode(), exchange.getBody());
+        return exchange.getStatusCode().is2xxSuccessful();
     }
 }
